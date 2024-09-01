ed03db9f062b8371bd6f76ca876cfe1160201c5b
Michal <m.jagodzinski1234@gmail.com>
2024-09-01 15:16:52 +0200

    image for messagediv

--- public/js/client.js
+++ public/js/client.js
@@ -160,12 +160,36 @@ ws.onmessage = (event) => {
 };
 
 function createMessageDiv(oponentName, character) {
-  // Create a new div element
+  // Create the main div element
   const messageDiv = document.createElement("div");
-  messageDiv.textContent = `${oponentName} - ${character}`;
   messageDiv.classList.add("message");
+  messageDiv.style.display = "flex";
+  messageDiv.style.flexDirection = "column";
+  messageDiv.style.alignItems = "center";
+  messageDiv.style.width = "300px";
+  messageDiv.style.borderRadius = "8px";
 
-  // Append the new div to the messages container
+
+  // Create a div for the background image
+  const imageDiv = document.createElement("div");
+  imageDiv.style.backgroundSize = "cover";
+  imageDiv.style.backgroundPosition = "center";
+  imageDiv.style.width = "280px";
+  imageDiv.style.height = "300px";
+  imageDiv.style.borderRadius = "8px";
+
+  if (character === "???????") {
+    imageDiv.style.backgroundImage = "url('/images/questionMarks.png')";
+  }
+
+  const textParagraph = document.createElement("p");
+  textParagraph.textContent = `${oponentName} - ${character}`;
+  textParagraph.style.textAlign = "center";
+  textParagraph.style.marginTop = "10px";
+
+  messageDiv.appendChild(imageDiv);
+  messageDiv.appendChild(textParagraph);
+
   messages.appendChild(messageDiv);
   console.log(`dodaje do diva ${oponentName} - ${character}`);
 }
